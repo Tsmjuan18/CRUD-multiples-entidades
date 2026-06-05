d4d7fe28310c89dea600ccba8efb26231a52ecd7
Tsmjuan18 <juanjosesantamaria18@gmail.com>
2026-06-05 14:36:30 -0500

    subo servicios

--- src/main/java/com/sena/database_connection/services/PostService.java
+++ src/main/java/com/sena/database_connection/services/PostService.java
@@ -1,7 +1,11 @@
 package com.sena.database_connection.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.sena.database_connection.entities.Post;
 import com.sena.database_connection.repositories.PostRepository;
 
 @Service
@@ -13,6 +17,54 @@ public class PostService {
     public PostService (PostRepository postRepository){
         this.postRepository= postRepository;
     }
+
+    public List<Post> getAllPost(){
+
+        return this.postRepository.findAll();
+
+    }
+
+    public Optional<Post> getIdPost(long id){
+
+        return this.postRepository.findById(id);
+    }
+
+    public Post create  (Post post){
+        return this.postRepository.save(post);
+    }
+
+    public Post update(Post post){
+        Optional<Post> postFound = this.getIdPost(post.getId());
+
+        if (postFound.isEmpty()) {
+
+            return null;
+            
+        }
+
+        return this.postRepository.save(post);
+    }
+
+    public Post delete (Long id){
+        Optional <Post> postFound = this.getIdPost(id);
+
+        if (postFound.isEmpty()) {
+
+            return null;
+            
+        }
+
+         this.postRepository.delete(postFound.get());
+
+         return postFound.get();
+
+    }
+
+    
+
+
+
+
 
 
     
--- src/main/java/com/sena/database_connection/services/ProfileService.java
+++ src/main/java/com/sena/database_connection/services/ProfileService.java
@@ -1,5 +1,76 @@
 package com.sena.database_connection.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.sena.database_connection.entities.Profile;
+import com.sena.database_connection.repositories.ProfileRepository;
+
+
+@Service
 public class ProfileService {
+
+    private ProfileRepository repository;
+
+    public ProfileService(ProfileRepository repository){
+
+        this.repository = repository;
+
+    }
+
+    public List<Profile> getAll(){
+
+        return this.repository.findAll();
+
+    }
+
+    public Optional<Profile> getId(Long id){
+
+        return this.repository.findById(id);
+
+    }
+
+    public Profile create (Profile profile){
+
+        return this.repository.save(profile);
+
+
+    }
+
+    public Profile update(Profile profile){
+
+        Optional<Profile> profileFound = this.getId(profile.getId());
+
+        if (profileFound.isEmpty()) {
+
+            return null;
+            
+        }
+
+        return this.repository.save(profile);
+
+    }
+
+    public Profile delete(Long id){
+
+        Optional<Profile> profileFound = this.getId(id);
+
+        if (profileFound.isEmpty()) {
+            
+            return null;
+        }
+
+         this.repository.delete(profileFound.get());
+         return profileFound.get();
+
+
+    }
+
+
+
+
+
     
 }
